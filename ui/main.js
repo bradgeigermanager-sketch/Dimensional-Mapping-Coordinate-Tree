7e07f0e3714230909b527f05e1aaae57ed4fe1a3
bradgeigermanager-sketch <brad.geiger.manager@gmail.com>
2026-06-15 21:05:11 -0600

    Add run function to handle mode selection

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,13 @@ export function loadData() {
     .then(res => res.json())
     .then(data => renderPoints(data));
 }
+function run() {
+  const mode = document.getElementById("mode").value;
+
+  fetch('../data/sample.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log("Mode:", mode);
+      renderPoints(data, mode);
+    });
+}
